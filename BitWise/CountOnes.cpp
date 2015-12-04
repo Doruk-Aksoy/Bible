28ceb374a64e46ca755befdd9538725205129e1c
Doruk Aksoy <Doruk-Aksoy@users.noreply.github.com>
2015-12-04 21:22:27 +0200

    Update CountOnes.cpp

--- BitWise/CountOnes.cpp
+++ BitWise/CountOnes.cpp
@@ -1,3 +1,13 @@
+    /*
+    PROBLEM STATEMENT:
+        
+        Find all set bits in an integer A.
+        
+    PARAMETERS:
+    
+        ((long long) unsigned) int A
+    */
+    
     int res = 0;
     while(A) {
         res += A & 1;
